ed30903aee2a284a8b2b708d7243d963a49335a7
Greg <gredelston@gmail.com>
2015-05-20 16:51:42 -0400

    cleanup

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth2').Strategy;
+
 var index  = require('./routes/index');
 var models = require('./models/model-manager');
 
 var app = express();
+var appDir = path.dirname(require.main.filename);
+console.log(appDir);
 
 // Application settings
 app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'main' }));
@@ -26,17 +31,34 @@ app.use(session({
 	saveUninitialized: true
 }));
 
+// Google OAuth2 authentication
+// var GOOGLE_CLIENT_ID = 0;
+// var GOOGLE_CLIENT_SECRET = 0;
+// passport.serializeUser(function(user, done) {
+// 	done(null, user);
+// });
+// passport.deserializeUser(function(user, done) {
+// 	done(null, user);
+// });
+// passport.use(new SpotifyStrategy({
+//     clientID: GOOGLE_CLIENT_ID,
+//     clientSecret: GOOGLE_CLIENT_SECRET,
+//     callbackURL: path.dirname(require.main.filename) + "/authed"
+//   },
+//   function(accessToken, refreshToken, profile, done) {
+//     // User.findOrCreate({ spotifyId: profile.id }, function (err, user) {
+//     //   return done(err, user);
+//     console.log(profile);
+//     var user = profile;
+//     user.token = accessToken;
+// 	return done(null, user);
+//   }
+// ));
+
 // Routes
-
+console.log("HNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNG");
+console.log(appDir);
 app.get('/', index.home);
-app.get('/login', index.login);
-app.get('/loggedIn', index.loggedIn);
-
-app.post('/loginSubmit', index.loginSubmit);
-app.post('/logoutSubmit', index.logoutSubmit);
-app.post('/newTwote', index.newTwote);
-
-app.delete('/deleteTwote', index.deleteTwote);
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
